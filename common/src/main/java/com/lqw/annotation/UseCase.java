bed6c506ee917d8b2c1e3343740cd38cf56657b4
quanwei.luo <quanwei.luo@qiaofang.local>
2021-03-30 18:02:22 +0800

    注解功能 学习，创建一个注解

---
+++ common/src/main/java/com/lqw/annotation/UseCase.java
@@ -0,0 +1,12 @@
+package com.lqw.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * @author LQW
+ * @date 2021-03-30 17:55
+ **/
+@Target(ElementType.ANNOTATION_TYPE)
+public @interface UseCase {
+}
