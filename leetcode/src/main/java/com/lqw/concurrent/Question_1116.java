f1560bb98d259f32c9d12d5ee750279143f691e4
lqw <751213750@qq.com>
2021-08-18 14:05:56 +0800

    leetCode-1116.打印零与奇偶数

--- leetcode/src/main/java/com/lqw/concurrent/Question_1116.java
+++ leetcode/src/main/java/com/lqw/concurrent/Question_1116.java
@@ -1,48 +1,76 @@
 package com.lqw.concurrent;
 
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.IntConsumer;
 
 public class Question_1116 {
-
     private int n;
 
     public Question_1116(int n) {
         this.n = n;
     }
-
-    private volatile int j=1;
-    private Semaphore zero=new Semaphore(0);
+    private Semaphore zero=new Semaphore(1);
     private Semaphore even=new Semaphore(0);
-    private Semaphore odd=new Semaphore(1);
+    private Semaphore odd=new Semaphore(0);
 
     // printNumber.accept(x) outputs "x", where x is an integer.
     public void zero(IntConsumer printNumber) throws InterruptedException {
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             zero.acquire();
             printNumber.accept(0);
+            if (i%2==0){
+                even.release();
+            }else {
+                odd.release();
+            }
         }
     }
 
     public void even(IntConsumer printNumber) throws InterruptedException {
-        for (; j <= n; ) {
-            zero.release();
+        for (int i=2;i<=n;i+=2) {
             even.acquire();
-            printNumber.accept(j);
-            j++;
-            odd.release();
+            printNumber.accept(i);
+            zero.release();
         }
     }
 
     public void odd(IntConsumer printNumber) throws InterruptedException {
-        for ( ; j <= n;  ) {
-            zero.release();
+        for (int i=1;i<=n;i+=2) {
             odd.acquire();
-            printNumber.accept(0);
-            j++;
-            even.release();
+            printNumber.accept(i);
+            zero.release();
         }
+    }
+
+    public static void main(String[] args) {
+        Question_1116 zeroEvenOdd=new Question_1116(12);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        executorService.execute(()-> {
+            try {
+                zeroEvenOdd.zero((int i)-> System.out.printf(i+""));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        executorService.execute(()-> {
+            try {
+                zeroEvenOdd.even((int i)-> System.out.printf(i+""));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        executorService.execute(()-> {
+            try {
+                zeroEvenOdd.odd((int i)-> System.out.printf(i+""));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
